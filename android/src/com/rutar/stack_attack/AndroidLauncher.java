5bade642dfd7dd90515f0b2c5217d2393a4bae6d
Rutar Andriy <RutarAndriy@gmail.com>
2022-05-13 15:23:24 +0300

    Generated project structure

---
+++ android/src/com/rutar/stack_attack/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rutar.stack_attack;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rutar.stack_attack.Stack_Attack;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Stack_Attack(), config);
+	}
+}
---
+++ desktop/src/com/rutar/stack_attack/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.rutar.stack_attack;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.rutar.stack_attack.Stack_Attack;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Stack_Attack");
+		new Lwjgl3Application(new Stack_Attack(), config);
+	}
+}
